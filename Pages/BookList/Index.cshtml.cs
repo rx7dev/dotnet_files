a27f29a869513e45ba5a8f73527fab0ccc657043
rx7dev <rx7dev@users.noreply.github.com>
2018-07-10 23:07:43 -0700

    Add files via upload

---
+++ Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookList_Razor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookList_Razor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+
+        private ApplicationDbContext _db;
+
+        [TempData]
+        public string Message { get; set; }
+
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public async Task OnGet()
+        {
+            Books = await _db.Books.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = _db.Books.Find(id);
+            _db.Books.Remove(book);
+            await _db.SaveChangesAsync();
+
+            Message = "Book Deleted Successfully!";
+            return RedirectToPage();
+
+        }
+    }
+}
